170cb2c3f4165df1e3d44711f0c7107f0593ca45
tanyabednyakova <tmbednyakova@gmail.com>
2023-01-19 16:26:47 +0300

    Добавьте файлы проекта.

---
+++ WindowsFormsApp12/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp12
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Form2 form2;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            form2 = new Form2();
+            form2.setTextToLabel(textBox1.Text);
+            form2.Visible = true;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            form2.setTextToLabel(textBox1.Text);
+        }
+    }
+}
